980612ac3072249a1c985b6477f5b9c27a7c05b0
Khushi Siddhapura <khushisiddhapura2010@gmail.com>
2026-05-10 02:51:58 +0530

    promo card resized

--- src/Components/PromoComponents/TextR.jsx
+++ src/Components/PromoComponents/TextR.jsx
@@ -1,7 +1,7 @@
 const TextR = () => {
   return (
     <div className="flex flex-col gap-6 justify-center items-center">
-        <p className="text-6xl">Natural <br /> & Healthy</p>
+        <p className="text-6xl">Natural & <br />Healthy</p>
         <p className="text-green-950">Healthy ingredients packed with nutrition for your daily lifestyle.</p>
     </div>
   )
