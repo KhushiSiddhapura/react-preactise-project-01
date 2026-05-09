e81fe0f7e3c06d06e7d1182f0e44f9373575d9d4
Khushi Siddhapura <khushisiddhapura2010@gmail.com>
2026-05-10 02:49:06 +0530

    promo card resized

--- src/Components/PromoComponents/CardL.jsx
+++ src/Components/PromoComponents/CardL.jsx
@@ -2,7 +2,7 @@ import TextL from "./TextL"
 
 const CardL = () => {
   return (
-    <div className="bg-green-100 h-fit flex items-center justify-between w-full rounded-3xl p-8 shadow-2xl shadow-green-950">
+    <div className="bg-green-100 h-90 flex items-center justify-between w-140 rounded-3xl p-8 shadow-2xl shadow-green-950">
         <img src="https://png.pngtree.com/png-vector/20230930/ourmid/pngtree-avocado-png-avocado-fruit-ai-generated-png-image_10153887.png" alt="Image" className="h-50" />
         <TextL />
     </div>
--- src/Components/PromoComponents/CardR.jsx
+++ src/Components/PromoComponents/CardR.jsx
@@ -2,7 +2,7 @@ import TextR from "./TextR"
 
 const CardR = () => {
   return (
-    <div className="bg-mauve-100 flex items-center justify-between rounded-3xl p-8 h-fit w-full shadow-2xl shadow-mauve-950">
+    <div className="bg-mauve-100 flex items-center justify-between rounded-3xl p-8 h-90 w-140 shadow-2xl shadow-mauve-950">
         <TextR />
         <img src="https://static.vecteezy.com/system/resources/thumbnails/044/813/725/small/cocoa-powder-in-wooden-bowl-isolated-on-transparent-background-png.png" alt="Image" className="h-50" />
     </div>
